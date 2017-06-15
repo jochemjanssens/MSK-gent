84c9cdc07cc564c22401e3ecd9bcf3420624b577
Jochem Janssens <janssens.jochem@gmail.com>
2017-06-15 22:11:04 +0200

    css fixes

--- src/js/components/BlogItemPreviewHome.jsx
+++ src/js/components/BlogItemPreviewHome.jsx
@@ -5,15 +5,6 @@ import {Link} from 'react-router-dom';
 const BlogItemPreviewHome = content => {
   return (
     <Link to={`/Blogpost/${content.id}`} className='blogItemHome'>
-      <article className='blogItem'>
-        <header className='blogItem-title'>
-          <h2>{content.title}</h2>
-        </header>
-        <p className='blogItem-text blogItem-text-preview'>{content.text}</p>
-        <div className='buttons'>
-           <p className='blogItem-detail'>Lees meer </p>
-        </div>
-      </article>
     <article className='blogItem desktop-blogs'>
       <header className='blogItem-title'>
         <h2>{content.title}</h2>
@@ -21,7 +12,6 @@ const BlogItemPreviewHome = content => {
       <p className='blogItem-text blogItem-text-preview'>{content.text}</p>
       <div className='buttons'>
          <p className='blogItem-detail'>Lees meer </p>
-        <Link to={`/Blog`} className='blogItem-all'>Ontdek meer blogs</Link>
       </div>
     </article>
     </Link>
