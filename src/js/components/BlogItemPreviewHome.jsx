a200bb41740b848fcb259c50ad935a6431c9f927
DieterVanHertbruggen <dietervanhertbruggen8@gmail.com>
2017-06-15 18:24:16 +0200

    css

--- src/js/components/BlogItemPreviewHome.jsx
+++ src/js/components/BlogItemPreviewHome.jsx
@@ -5,7 +5,6 @@ import {Link} from 'react-router-dom';
 const BlogItemPreviewHome = content => {
   return (
     <Link to={`/Blogpost/${content.id}`} className='blogItemHome'>
-<<<<<<< HEAD
       <article className='blogItem'>
         <header className='blogItem-title'>
           <h2>{content.title}</h2>
@@ -15,8 +14,6 @@ const BlogItemPreviewHome = content => {
            <p className='blogItem-detail'>Lees meer </p>
         </div>
       </article>
-=======
->>>>>>> abf71b26ffe010f0675ea3a0834c9aed0ffa335e
     <article className='blogItem desktop-blogs'>
       <header className='blogItem-title'>
         <h2>{content.title}</h2>
