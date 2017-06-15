383c0d113a5e3a058cb15e5ab026afa3c75a6d4f
Jochem Janssens <janssens.jochem@gmail.com>
2017-06-15 13:37:02 +0200

    🙇  #18 artiest kiezer volledige naam

--- src/js/containers/ChooseArtist.jsx
+++ src/js/containers/ChooseArtist.jsx
@@ -61,7 +61,7 @@ const ChooseArtist = ({store}) => {
         <header className='chooseArtist-title'>
           <h2><span>Welke</span><br /><span>kunstenaar</span><span>kies</span><br /><span>je</span><span>?</span></h2>
         </header>
-        <p className='chooseArtist-artistName'>{artistsList[currentArtistId]}</p>
+        <p className='chooseArtist-artistName'>{artistData.firstname} {artistData.name}</p>
         <section className='slider'>
           <div onClick={() => handleArtistClick(`back`)} className='slideritem-inactive'>
             <img className='slider-img' src={`assets/img/${artistsList[numbers[0]]}_slider.png`} width='110' height='110' />
