5a9e205ed05cb012c16c7d8f11060ed0de9504cd
Jochem Janssens <janssens.jochem@gmail.com>
2017-06-15 13:34:24 +0200

    👥 meer blogitems op een groter scherm

--- src/js/containers/Home.jsx
+++ src/js/containers/Home.jsx
@@ -18,7 +18,7 @@ const Home = ({store}) => {
 
   console.log(artistData);
 
-  if (blogItems.length !== 0) {
+  if (blogItems.length === 1) {
     return (
       <section className='home'>
         <header className='header'>
@@ -43,6 +43,60 @@ const Home = ({store}) => {
                 {...blogItems[latestBlogItem]}
                 key={blogItems[latestBlogItem].id}
               />
+            </div>
+
+            <article className='socialmedia'>
+              <header className='title'>
+                <h2>Liked of volgt mij ne keer</h2>
+              </header>
+              <ul className='socialmedia-list'>
+                <li>
+                  <a href='https://www.facebook.com/mskgent/'><img src='assets/svg/facebook.svg' alt='logo facebook' width='19' height='36' /></a>
+                </li>
+                <li>
+                  <a href='https://twitter.com/mskgent?lang=nl'><img src='assets/svg/twitter.svg' alt='logo twitter' width='34' height='29' /></a>
+                </li>
+                <li>
+                  <a href='https://www.instagram.com/mskgent/'><img src='assets/svg/instagram.svg' alt='logo instagram' width='34' height='34' /></a>
+                </li>
+              </ul>
+            </article>
+          </section>
+          <Navigation />
+        </main>
+      </section>
+    );
+  } else if (blogItems.length !== 0) {
+    return (
+      <section className='home'>
+        <header className='header'>
+          <h1 className='hidden'>MSK tour</h1>
+          <img src='assets/svg/logo.svg' alt='logoSMK' width='104' height='44' className='header-logo' />
+          <div className='header-comp'>
+            <img src='assets/img/header-ensor.png' alt='beeld Ensor' width='170' height='140' className='header-beeld' />
+            <p className='header-text'>komt ge nog naar het MSK ? {(checkOpening() === true) ? `Ik ben er al zenne.` : `vandaag zijn we niet open`}</p>
+          </div>
+        </header>
+        <main>
+          <section>
+            <header className='artist'>
+              <h2 className='artist-name'><span>{artistData.firstname}</span><span>{artistData.name}</span></h2>
+              <p className='artist-liveyears'>{artistData.liveyears}</p>
+            </header>
+            <div>
+              <header className='title'>
+                <h2>Bekijk mijn avonturen in Gent</h2>
+              </header>
+              <BlogItemPreviewHome
+                {...blogItems[latestBlogItem]}
+                key={blogItems[latestBlogItem].id}
+              />
+              <div className='secondBlogItem'>
+                <BlogItemPreviewHome
+                  {...blogItems[latestBlogItem - 1]}
+                  key={blogItems[latestBlogItem - 1].id}
+                />
+              </div>
             </div>
 
             <article className='socialmedia'>
